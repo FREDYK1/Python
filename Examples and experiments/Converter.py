1b4c763f6809eabb4654851c5d70df18b0901a32
FREDYK1 <fkwekukankam@gmail.com>
2024-06-09 20:31:27 +0000

    Implement The Convert Button

--- Examples and experiments/Converter.py
+++ Examples and experiments/Converter.py
@@ -1,11 +1,22 @@
 import FreeSimpleGUI as Gui
+import ConverterBackend
 
 FeetLabel = Gui.Text("Enter Feet:")
 InchesLabel = Gui.Text("Enter Inches")
-FeetBox = Gui.Input()
-InchesBox = Gui.Input()
+FeetBox = Gui.Input(tooltip="How many feets are you?", key="Feet")
+InchesBox = Gui.Input(tooltip="How many inches are you?", key="Inches")
 ConvertButton = Gui.Button("Convert")
+MeterText = Gui.Text("", key="CompletionText", text_color="blue")
 
-window = Gui.Window("CONVERTOR", layout=[[FeetLabel, FeetBox], [InchesLabel, InchesBox], [ConvertButton]])
-window.read()
+window = Gui.Window("CONVERTOR", layout=[[FeetLabel, FeetBox],
+                                         [InchesLabel, InchesBox],
+                                         [ConvertButton, MeterText]])
+while True:
+    event, value = window.read()
+    print(event, value)
+    feets = float(value["Feet"])
+    inches = float(value["Inches"])
+    meter = ConverterBackend.conv(feets, inches)
+    window["CompletionText"].update(value=f"{feets}feets and {inches}inches is {meter}m")
+
 window.close()
---
+++ Examples and experiments/ConverterBackend.py
@@ -0,0 +1,5 @@
+
+def conv(feets, inches):
+    meter = feets * 0.3048 + inches * 0.0254
+    return meter
+
