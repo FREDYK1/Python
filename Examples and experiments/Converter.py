e38cb1f42f78f8df54b7a864a3791c348aee809e
FREDYK1 <fkwekukankam@gmail.com>
2024-06-14 17:37:50 +0000

    Add and implement Exit button

--- Examples and experiments/Converter.py
+++ Examples and experiments/Converter.py
@@ -7,16 +7,22 @@ FeetBox = Gui.Input(tooltip="How many feets are you?", key="Feet")
 InchesBox = Gui.Input(tooltip="How many inches are you?", key="Inches")
 ConvertButton = Gui.Button("Convert")
 MeterText = Gui.Text("", key="CompletionText", text_color="blue")
+ExitButton = Gui.Button("Exit")
 
 window = Gui.Window("CONVERTOR", layout=[[FeetLabel, FeetBox],
                                          [InchesLabel, InchesBox],
-                                         [ConvertButton, MeterText]])
+                                         [ConvertButton, ExitButton, MeterText]])
 while True:
     event, value = window.read()
-    print(event, value)
-    feets = float(value["Feet"])
-    inches = float(value["Inches"])
-    meter = ConverterBackend.conv(feets, inches)
-    window["CompletionText"].update(value=f"{feets}feets and {inches}inches is {meter}m")
+    match event:
+        case "Convert":
+            feets = float(value["Feet"])
+            inches = float(value["Inches"])
+            meter = ConverterBackend.conv(feets, inches)
+            window["CompletionText"].update(value=f"{feets}feets and {inches}inches is {meter}m")
+        case "Exit":
+            break
+        case Gui.WINDOW_CLOSED:
+            break
 
 window.close()
