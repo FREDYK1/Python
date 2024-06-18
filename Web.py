3b791170792373cda6555df168790ec52d04d95a
FREDYK1 <fkwekukankam@gmail.com>
2024-06-18 15:01:09 +0000

    Implement "Delete Completed Todo" feature

--- Web.py
+++ Web.py
@@ -15,7 +15,13 @@ sl.subheader("Increase Your Productivity today.")
 sl.write("Your todolist for today are;")
 
 
-for todo in todos:
-    sl.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = sl.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        mainFunctions.write_todos(todos)
+        del sl.session_state[todo]
+        sl.experimental_rerun()
+
 
 sl.text_input("", placeholder="Enter a Todo...", key="new_todo", on_change=add_todo)
