7759502e17369015084a206dfb6bd3bcc22f7f2e
FREDYK1 <fkwekukankam@gmail.com>
2024-06-18 21:52:16 +0000

    Add upload a picture feature

--- Examples and experiments/GrayscalePic.py
+++ Examples and experiments/GrayscalePic.py
@@ -1,6 +1,11 @@
 import streamlit as sl
 from PIL import Image
 
+uploaded_image = sl.file_uploader("Upload Image")
+if uploaded_image:
+    image = Image.open(uploaded_image)
+    grayscale = image.convert("L")
+    sl.image(grayscale)
 
 with sl.expander("Start Camera"):
     camera_image = sl.camera_input("Camera")
